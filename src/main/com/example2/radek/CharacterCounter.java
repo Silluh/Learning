9509e0857d2544d2a6cf3d8563cf21ca8a742c86
Radek Hadamčík <radekhadamcik@gmail.com>
2022-02-17 22:27:37 +0100

    New characterCounter application

---
+++ src/main/com/example2/radek/CharacterCounter.java
@@ -0,0 +1,24 @@
+package main.com.example2.radek;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class CharacterCounter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert word, which you want to count: ");
+        String Word = scanner.nextLine();
+        if(Word.length() > 0){
+            char[] chars = Word.toCharArray();
+            System.out.println(chars);
+            for(int i = 0; i < Word.length(); i++){
+                System.out.println(chars[i] + " " + getOccurence(chars[i], Word));
+            }
+        }
+    }
+    static long getOccurence(char chars, String word){
+        long count = 0;
+        IntStream Input = word.chars();
+        count = Input.filter(i -> i == chars).count();
+        return count;
+    }
+}
