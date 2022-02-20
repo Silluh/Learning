6a979d61b9798d42081e301a7834017956289c81
Radek Hadamčík <radekhadamcik@gmail.com>
2022-02-20 01:01:31 +0100

    Spacing

--- src/main/com/example1/radek/RandomNumberApplication.java
+++ src/main/com/example1/radek/RandomNumberApplication.java
@@ -2,10 +2,14 @@ package main.com.example1.radek;
 import java.util.Random;
 
 public class RandomNumberApplication {
+
     public static void main(String[] args) {
+
         randomGenerator(5);
     }
+
     static void randomGenerator(int numbers){
+        
         Random rng = new Random();
         for(int i = 0; i < numbers; i++) {
             System.out.print(rng.nextInt(100) + " " );
