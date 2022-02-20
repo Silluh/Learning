f5cc880dab32c3b615e409055cfa9b7db21d4ed4
Daniel Zazo <daniel.zazo@etnetera.eu>
2022-02-20 01:55:18 +0100

    My solution

---
+++ src/main/com/example2/daniel/CharCounterApplication.java
@@ -0,0 +1,48 @@
+package main.com.example2.daniel;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CharCounterApplication {
+
+    public static void main(String[] args) {
+
+        String text = getEnteredText();
+        Map<Character, Integer> parsedText = parseTextByEachCharacter(text);
+        printResult(parsedText);
+
+        // to cele lze zapsat takto, ale neni to citelne
+        // printResult(parsedTextByEachCharacter(getEnteredText()));
+    }
+
+    private static String getEnteredText() {
+
+        System.out.print("Write some text: ");
+        Scanner scanner = new Scanner(System.in);
+
+        return scanner.nextLine();
+    }
+
+    private static Map<Character, Integer> parseTextByEachCharacter(String text) {
+
+        var result = new HashMap<Character, Integer>();
+
+        for(char character : text.toCharArray()) {
+            if(!result.containsKey(character)) {
+                result.put(character, 0);
+            }
+            int currentCount = result.get(character);
+            result.put(character, ++currentCount);
+        }
+
+        return result;
+    }
+
+    private static void printResult(Map<Character, Integer> parsedText) {
+
+        for(Map.Entry<Character,Integer> entry : parsedText.entrySet()) {
+            System.out.println(entry.getKey() + "\t" + entry.getValue());
+        }
+    }
+}
