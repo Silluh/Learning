180970066083c4a22e0dd0d1b8e2adb5e365a107
Radek Hadamčík <radekhadamcik@gmail.com>
2022-02-21 15:34:31 +0100

    Variable renaming, boxed remove přejmenování proměnné, odebrán boxed

--- src/main/com/example3/radek/ArrayRandomNumberGeneratorApplication.java
+++ src/main/com/example3/radek/ArrayRandomNumberGeneratorApplication.java
@@ -10,24 +10,23 @@ public class ArrayRandomNumberGeneratorApplication {
         int[] array = new int[49];
         int[] array2 = new int[6];
         array = getFilledArray(false, array);
-        Arrays.stream(array).boxed().forEach(result -> System.out.print(result + " "));
+        Arrays.stream(array).forEach(result -> System.out.print(result + " "));
         System.out.println();
         array2 = getFilledArray(true, array2);
-        Arrays.stream(array2).boxed().forEach(result -> System.out.print(result + " "));
-
+        Arrays.stream(array2).forEach(result -> System.out.print(result + " "));
     }
 
     static int[] getFilledArray(boolean fillRandom, int[] array) {
 
         for (int i = 0; i < array.length; i++) {
-            array[i] = fillRandom ? getRNG() + 1 : i + 1;
+            array[i] = fillRandom ? getRandomNumber() + 1 : i + 1;
         }
         return array;
     }
 
-    static int getRNG() {
+    static int getRandomNumber() {
 
-        SecureRandom rng = new SecureRandom();
-        return rng.nextInt(49);
+        SecureRandom randomNumber = new SecureRandom();
+        return randomNumber.nextInt(49);
     }
 }
