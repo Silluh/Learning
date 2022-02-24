8ded219d7c5d07b5a3ed05c4cce49e3228b62229
Silluh <radekhadamcik@seznam.cz>
2022-02-24 16:14:44 +0100

    Creating new calculator object, creating basic calcutation methods, calculator inicialization, printing results Vytvoření nového objektu kalkulačky, vytvoření metod základních počtů, inicializace kalkulačky, tištění výsledků

---
+++ src/main/com/example007/radek/Calculator.java
@@ -0,0 +1,28 @@
+package main.com.example007.radek;
+
+public class Calculator {
+
+    double plus(double value, double value2){
+
+        return value + value2;
+    }
+
+    double minus(double value, double value2){
+
+    return value - value2;
+    }
+
+    double multi(double value, double value2){
+
+        return value * value2;
+    }
+
+    double divide(double value, double value2){
+
+        if(value2 == 0) {
+            System.out.println("You can't divide by zero!");
+            System.exit(0);
+        }
+        return value / value2;
+    }
+}
---
+++ src/main/com/example007/radek/basicCalculatorApplication.java
@@ -0,0 +1,13 @@
+package main.com.example007.radek;
+
+public class basicCalculatorApplication {
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.plus(1,2));
+        System.out.println(calculator.minus(1,2));
+        System.out.println(calculator.multi(1,2));
+        System.out.println(calculator.divide(1,0));
+    }
+}
