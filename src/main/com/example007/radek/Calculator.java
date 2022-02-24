c67f220071599a7374eb5f9e714e43cf79288749
Silluh <radekhadamcik@seznam.cz>
2022-02-24 16:15:03 +0100

    Formating

--- src/main/com/example007/radek/Calculator.java
+++ src/main/com/example007/radek/Calculator.java
@@ -2,24 +2,24 @@ package main.com.example007.radek;
 
 public class Calculator {
 
-    double plus(double value, double value2){
+    double plus(double value, double value2) {
 
         return value + value2;
     }
 
-    double minus(double value, double value2){
+    double minus(double value, double value2) {
 
-    return value - value2;
+        return value - value2;
     }
 
-    double multi(double value, double value2){
+    double multi(double value, double value2) {
 
         return value * value2;
     }
 
-    double divide(double value, double value2){
+    double divide(double value, double value2) {
 
-        if(value2 == 0) {
+        if (value2 == 0) {
             System.out.println("You can't divide by zero!");
             System.exit(0);
         }
--- src/main/com/example007/radek/basicCalculatorApplication.java
+++ src/main/com/example007/radek/basicCalculatorApplication.java
@@ -5,9 +5,9 @@ public class basicCalculatorApplication {
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
-        System.out.println(calculator.plus(1,2));
-        System.out.println(calculator.minus(1,2));
-        System.out.println(calculator.multi(1,2));
-        System.out.println(calculator.divide(1,0));
+        System.out.println(calculator.plus(1, 2));
+        System.out.println(calculator.minus(1, 2));
+        System.out.println(calculator.multi(1, 2));
+        System.out.println(calculator.divide(1, 0));
     }
 }
