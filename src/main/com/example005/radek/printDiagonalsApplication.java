1256cf63c86ca512dd4b28fbfaff988f0e5cabd6
Radek Hadamčík <radekhadamcik@gmail.com>
2022-02-25 13:52:09 +0100

    Delete return from catch method, inicialization of new variable for return, methods for prints, new printing statement Smazání návratové hodnoty z catch, inicializace nové proměnné pro návratovou hodnotu, metody pro zobrazení, nové podmínky zobrazení

--- src/main/com/example005/radek/printDiagonalsApplication.java
+++ src/main/com/example005/radek/printDiagonalsApplication.java
@@ -17,28 +17,30 @@ public class printDiagonalsApplication {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter size of diagonal: ");
+        int value = 0;
         try {
-            return scanner.nextInt();
+            value = scanner.nextInt();
+
         } catch (InputMismatchException nfe) {
             System.out.println("You have to insert numeric value!");
             System.exit(0);
-            return 0;
         }
+        return value;
     }
 
     static void printDiagonals(DiagonalType type, int size) {
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j <= size; j++) {
+        for (int row = 0; row < size; row++) {
+            for (int column = 0; column < size; column++) {
                 switch (type) {
                     case LEFT:
-                        System.out.print(j == i ? "x" : " ");
+                        printLeft(column, row);
                         break;
                     case RIGHT:
-                        System.out.print(j == size - i ? "x" : " ");
+                        printRight(size, column, row);
                         break;
                     case BOTH:
-                        System.out.print(j == i || j == size - i ? "x" : " ");
+                        printBoth(size, column, row);
                         break;
 
                 }
@@ -47,6 +49,21 @@ public class printDiagonalsApplication {
         }
         System.out.println();
 
+    }
+
+    static void printLeft(int column, int row) {
+
+        System.out.print(column == row ? "x" : " ");
+    }
+
+    static void printRight(int size, int column, int row) {
+
+        System.out.print(column == size - row - 1 ? "x" : " ");
+    }
+
+    static void printBoth(int size, int column, int row) {
+
+        System.out.print(column == row || column == size - row - 1 ? "x" : " ");
     }
 
 }
